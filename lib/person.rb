a3cf864e078db2f6ff866b052461d9f84007a4c7
sofiaversoza <sofversoza@gmail.com>
2022-01-16 15:55:00 -0700

    commit tree 20d2228569e64ca12a0c06f3eba12a29f16c540b

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,14 @@
-# your code goes here
+class Person
+    attr_reader :name, :hygiene
+    attr_accessor :bank_account, :happiness
+    
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    
+    
+end
